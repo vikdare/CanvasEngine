357bb56b929222884776c962959e13183b7817af
Alexey Klimko <aklimko@sugarcrm.com>
2014-02-26 17:34:01 +0300

    init change

--- samples/hit/js/main.js
+++ samples/hit/js/main.js
@@ -9,38 +9,60 @@ canvas.Scene.New({
 
     materials: {
         images: {
-            "box": "images/crate.jpg"
+            "black_box": "images/black.png",
+            "green_box": "images/green.png",
+            "silver_box": "images/silver.png"
         }
     },
     
     ready: function(stage) {
         var self = this;
         
-        function _entity(x, y) {
+        function _black(x, y) {
             var entity = Class.New("Entity", [stage]);
-            entity.rect(128);
+            entity.rect(641);
             entity.position(x, y);
-            entity.el.drawImage("box");
+            entity.el.drawImage("black_box");
+            stage.append(entity.el);
+            return entity;
+        }
+
+        function _green(x, y) {
+            var entity = Class.New("Entity", [stage]);
+            entity.rect(641);
+            entity.position(x, y);
+            entity.el.drawImage("green_box");
+            stage.append(entity.el);
+            return entity;
+        }
+
+        function _silver(x, y) {
+            var entity = Class.New("Entity", [stage]);
+            entity.rect(641);
+            entity.position(x, y);
+            entity.el.drawImage("silver_box");
             stage.append(entity.el);
             return entity;
         }
         
-        this.box1 = _entity(0, 50);
-        this.box2 = _entity(200, 50);
+//        this.box1 = _black(0, 0);
+//        this.box3 = _green(0, 0);
+        this.box2 = _silver(0, 0);
+        //this.box2 = _entity(200, 50);
         
     },
     render: function(stage) {
 
-        this.box1.move(2); // Position X + 2
+//        this.box1.move(2); // Position X + 2
         
-        this.box2.hit([this.box1], function(state, el) {
-            if (state == "over") {
-                el.opacity = 0.5;
-            }
-            else {
-                el.opacity = 1;
-            }
-        });
+//        this.box2.hit([this.box1], function(state, el) {
+//            if (state == "over") {
+//                el.opacity = 0.5;
+//            }
+//            else {
+//                el.opacity = 1;
+//            }
+//        });
         stage.refresh();
     }
 });
